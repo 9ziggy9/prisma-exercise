b14bf98bb8b0e499a2c0bfeaafb2c6628263c817
9ziggy9 <davidarogers@protonmail.com>
2022-11-11 16:51:24 -0800

    testing additional models

--- main.js
+++ main.js
@@ -9,11 +9,18 @@ const orm = new MappingClient(new sqlite3.Database("./db/test.db", err => {
 const Pet = new Model("Pet", [
   {name:"id", type:"INTEGER", pk:true},
   {name:"name", type:"TEXT", nullable:false},
-  {name:"species", type:"TEXT", unique:true},
+  {name:"species", type:"TEXT"},
   {name:"breed", type:"TEXT"},
   {name:"owner_id", type:"INTEGER"}
 ]);
 
-orm.registerModels(Pet);
+const Owner = new Model("Owner", [
+  {name: "id", type:"INTEGER", pk:true},
+  {name: "name", type:"TEXT", nullable:false},
+  {name: "phone", type:"INTEGER", nullable:false, unique:true},
+  {name: "address", type:"TEXT"}
+]);
+
+orm.registerModels(Pet, Owner);
 orm.init();
 orm.exit();
